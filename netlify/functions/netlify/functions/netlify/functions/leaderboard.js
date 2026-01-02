4689b1637e17e01198cc6fd98d529ee63f088555
ANKIT GUPTA <61156597+ankitgupta1020@users.noreply.github.com>
2026-01-02 12:34:54 +0530

    Refactor leaderboard function response format

--- netlify/functions/netlify/functions/netlify/functions/leaderboard.js
+++ netlify/functions/netlify/functions/netlify/functions/leaderboard.js
@@ -1,14 +1,17 @@
-// leaderboard.js
 const SUPA_URL = process.env.SUPA_URL;
 const SUPA_KEY = process.env.SUPA_SERVICE_ROLE_KEY;
 
-exports.handler = async function(event) {
-  const res = await fetch(`${SUPA_URL}/rest/v1/weekly_leaderboard?select=id,display_name,city,weekly_total,week_key&order=weekly_total.desc&limit=10`, {
+exports.handler = async function() {
+  const res = await fetch(`${SUPA_URL}/rest/v1/weekly_leaderboard?select=id,display_name,city,weekly_total&order=weekly_total.desc&limit=10`, {
     headers: {
       'apikey': SUPA_KEY,
       'Authorization': `Bearer ${SUPA_KEY}`
     }
   });
   const data = await res.json();
-  return { statusCode: res.status, body: JSON.stringify(data) };
+  return { 
+    statusCode: 200, 
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data) 
+  };
 };
